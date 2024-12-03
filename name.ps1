ce2c3fa8cea864b65d12c44e7e9a781498a0621a
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-12-03 09:52:50 +0100

    Update name.ps1

--- name.ps1
+++ name.ps1
@@ -1,1 +1,17 @@
+# Define the new server name
+$NewServerName = "MyNewServerName"
 
+# Get the current computer name
+$CurrentServerName = (Get-WmiObject Win32_ComputerSystem).Name
+
+# Check if the new name is already applied
+if ($CurrentServerName -eq $NewServerName) {
+    Write-Host "The server name is already set to $NewServerName."
+} else {
+    # Change the computer name
+    Rename-Computer -NewName $NewServerName -Force
+
+    # Notify the user and reboot the system
+    Write-Host "Server name changed to $NewServerName. The server will now reboot."
+    Restart-Computer -Force
+}
