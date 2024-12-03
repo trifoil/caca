f57c87ae2311dd4ff4d70bbe094f839c2342d4bc
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-12-03 14:50:13 +0100

    Update table.ps1

--- table.ps1
+++ table.ps1
@@ -1,4 +1,5 @@
-Install-Module -Name ImportExcel -Scope CurrentUser
+# Install the ImportExcel module if not already installed
+Install-Module -Name ImportExcel -Scope CurrentUser -Force
 
 # Import the ImportExcel module
 Import-Module ImportExcel
@@ -9,10 +10,25 @@ $inputFilePath = "Employes.xlsx"
 # Read the "Liste3" sheet from the Excel file
 $sheetData = Import-Excel -Path $inputFilePath -WorksheetName "Liste3"
 
-# Convert all letters to lowercase
+# Function to remove diacritics
+function Remove-Diacritics {
+    param (
+        [string]$inputString
+    )
+    $normalizedString = $inputString.Normalize( [System.Text.NormalizationForm]::FormD )
+    $stringBuilder = New-Object System.Text.StringBuilder
+    $normalizedString.ToCharArray() | ForEach-Object {
+        if ( [System.Globalization.CharUnicodeInfo]::GetUnicodeCategory($_) -ne [System.Globalization.UnicodeCategory]::NonSpacingMark ) {
+            $null = $stringBuilder.Append($_)
+        }
+    }
+    return $stringBuilder.ToString().Normalize( [System.Text.NormalizationForm]::FormC )
+}
+
+# Convert all letters to lowercase and remove diacritics
 $sheetDataLower = $sheetData | ForEach-Object {
     $_.PSObject.Properties | ForEach-Object {
-        $_.Value = $_.Value.ToString().ToLower()
+        $_.Value = (Remove-Diacritics -inputString $_.Value.ToString()).ToLower()
     }
     $_
 }
@@ -20,6 +36,7 @@ $sheetDataLower = $sheetData | ForEach-Object {
 # Display the modified sheet in the console
 $sheetDataLower | Format-Table -AutoSize
 
+# Define the path to the output CSV file
 $outputFilePath = "output.csv"
 
 # Save the modified sheet to a CSV file
