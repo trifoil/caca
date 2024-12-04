7b955e1af1fefee06635f2a7985ea41400cb025d
TiasMat16 <120423041+Mattias16-beep@users.noreply.github.com>
2024-12-04 14:58:24 +0100

    Update testOUSOUSOU.ps1

--- testOUSOUSOU.ps1
+++ testOUSOUSOU.ps1
@@ -1,8 +1,34 @@
+# Création d'un script permettant de créer des USERS sur l'AD
+# 1. Création des Groupes Globaux
+# 2. Création des Groupes Locaux
+# 3. Ajouts des utilisateurs du fichier CSV dans les Groupes Globaux
+
+# Génération des mdp
+function Generate-RandomPassword {
+    param (
+        [int]$length = 7
+    )
+
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?"
+    $password = -join ((1..$length) | ForEach-Object { $chars[(Get-Random -Maximum $chars.Length)] })
+    return $password
+}
+$randomPassword = Generate-RandomPassword
+Write-Output "Mot de passe généré : $randomPassword"
+
 # Création des OU 
 $csv = Import-Csv -Path "C:\FUCKONEDRIVE\ProjetWinServ\SCRIPT\output.csv"
 foreach ($line in $csv) {
     $ou = $line.ou
-    New-ADOrganizationalUnit -Name $ou -Path "DC=belgique,DC=lan"
+    $ou_path = "OU=$ou,DC=belgique,DC=lan"
+    New-ADOrganizationalUnit -Name $ou -Path $ou_path
+
+    #Check si l'OU existe déjà
+    if (Get-ADOrganizationalUnit -Filter {Name -eq $ou} -SearchBase $ou_path) {
+        Write-Output "L'OU $ou existe déjà"
+    } else {
+        Write-Output "L'OU $ou a été créé"
+    }
 }
 
 # Création des sous OU => récuperer depuis le CSV
@@ -11,4 +37,12 @@ foreach ($line in $csv) {
     $ou = $line.ou
     $sousou = $line.departement
     New-ADOrganizationalUnit -Name $sousou -Path "OU=$ou,DC=belgique,DC=lan"
+
+    #Check si le SOUS-OU existe déjà
+    if (Get-ADOrganizationalUnit -Filter {Name -eq $sousou} -SearchBase "OU=$ou,DC=belgique,DC=lan") {
+        Write-Output "Le SOUS-OU $sousou existe déjà"
+    } else {
+        Write-Output "Le SOUS-OU $sousou a été créé"
+    }
 }
+
